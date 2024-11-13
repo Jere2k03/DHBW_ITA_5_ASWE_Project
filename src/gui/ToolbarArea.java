c41bebe0a757ba9c76954b1457ab688677dc3223
Jeremias Matt <je.ma.03@web.de>
2024-11-13 20:52:22 +0100

    removed unnecessary buttons of Toolbar Area and its code

--- src/gui/ToolbarArea.java
+++ src/gui/ToolbarArea.java
@@ -1,6 +1,5 @@
 package gui;
 
-import control.ProjectHandling;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Separator;
@@ -46,25 +45,14 @@ public class ToolbarArea extends ToolBar {
      */
     public ToolbarArea() {
         // initialize buttons
-        Button openProjectButton = new Button("Open Project");
-        Button newFileButton = new Button("New File");
-        Button saveFileButton = new Button("Save File");
-        Button saveFileAsButton = new Button("Save File as");
         Button settingsButton = new Button("Settings");
         Button aboutButton = new Button("About");
         Button infoButton = new Button("Info");
 
         // add action listeners to the buttons
-        openProjectButton.setOnAction(e -> ProjectHandling.openProject());
-        newFileButton.setOnAction(e -> ProjectHandling.newFile());
         infoButton.setOnAction(e -> showInfoDialog());
 
         // add all buttons to the toolbar
-        this.getItems().add(openProjectButton);
-        this.getItems().add(newFileButton);
-        this.getItems().add(saveFileButton);
-        this.getItems().add(saveFileAsButton);
-        this.getItems().add(new Separator());
         this.getItems().add(settingsButton);
         this.getItems().add(aboutButton);
         this.getItems().add(infoButton);
