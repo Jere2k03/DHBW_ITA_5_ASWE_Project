578eb06a3acf0bc9ad3947bc5a1bc97c9a8867f9
Jeremias Matt <je.ma.03@web.de>
2024-11-14 14:45:20 +0100

    code cleanup

--- src/gui/ToolbarArea.java
+++ src/gui/ToolbarArea.java
@@ -2,7 +2,6 @@ package gui;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ToolBar;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
